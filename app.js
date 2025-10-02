7020c72d9f800d19cd1a74d6e26a0d3cd31f1a3b
RafaaDomingues <rafaellycaroline199@gmail.com>
2025-10-02 19:23:38 -0300

    update

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
-const imagem = document.querySelectorAll('img');//seleciona todas as imgs
+const img = document.querySelectorAll('img');//seleciona todas as imgs
 let trocando = false;
-const img1 = [
-    "d1.jpg",
-    "d2.jpg",
-    "d3.jpg",
-    "dean_principal.jpg",
-    "d4.jpg",
-    "d5.jpg",
-    "d6.jpg"
-];
 
-const img2 = [
-    "d7.jpg",
-    "d8.jpg",
-    "d9.jpg",
-    "d10.jpg",
-    "d11.jpg",
-    "d12.jpg",
-    "d13.jpg"
-]
 
 function mudarImagens() {
-    for (let d = 0; d > imagens.length; d++) {
-        if (!trocando) {
-            imagens[d].src = img2[d];
-        } else {
-            imagens[d].src = img1[i];
+     if (trocando === false) {
+        for (let d = 1; d < img.length; d++) {
+            img[d].src = `d${d + 8}.jpg`
         }
-        trcoando = !trocando;
+        trocando = true;
+    } else {
+        for (let d = 1; d < img.length; d++) {
+            img[d].src = `d${d + 1}.jpg`
+        }
+        trocando=false;
     }
 
-}
 
-document.getElementById('botao').addEventListener('click', mudarImagens);
+}
+   
+
